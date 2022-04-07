75beb98721795954bd896b4b291511b77def1867
huwenqing0606 <huwenqing.pku@gmail.com>
2022-04-07 17:49:47 -0500

    .

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -20,8 +20,12 @@ fn main() {
     
         println!("You guessed: {}", guess);
     
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-            match guess.cmp(&secret_number) {
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) =>  {println!("Not a number!"); continue;},
+        };
+
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => { println!("You win!"); break;},
