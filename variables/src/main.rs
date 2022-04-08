d6964669431a6195c98bef9bb779a55c9d05b00c
huwenqing0606 <huwenqing.pku@gmail.com>
2022-04-08 14:38:47 -0500

    .

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
@@ -13,4 +15,44 @@ fn main() {
     let spaces = "   ";
     let spaces = spaces.len();
     println!("spaces = {}", spaces);
+
+    // various other functions mentioned in this chapter
+    fn_array(1);
+    statements_expression();
+    println!("{}", return_value(1));
+}
+
+fn fn_array(x: i32) {
+    let a = [1, 2, 3, 4, 5];
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is : {}, add x is {}", 
+        index, element, element+x
+    );
+}
+
+fn statements_expression() {
+    let y={
+        let x = 3;
+        x+1
+    };
+    println!("y={}",y);
+}
+
+fn return_value(x: u32)->u32{
+    return x+1;
 }
