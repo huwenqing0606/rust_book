3425e15a14b70038a0c4dffa058d132feac0514c
huwenqing0606 <huwenqing.pku@gmail.com>
2022-04-08 12:12:32 -0500

    .

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("3 hours is {} seconds", THREE_HOURS_IN_SECONDS);
+
+    let x = 5;
+    let x = x + 1;
+    println!("x = {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("spaces = {}", spaces);
 }
