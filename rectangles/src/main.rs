84e24afcd52ab2196e341feecd7b00c10038996e
huwenqing0606 <huwenqing.pku@gmail.com>
2022-04-13 11:02:59 -0500

    .

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -10,8 +10,20 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
     println!("area is {}", area(&rect1));
-    println!("rectangle is {}, area is {}", rect1.width, rect1.area());
+    println!("rectangle1 is {}, area is {}", rect1.width, rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 
@@ -24,4 +36,8 @@ impl Rectangle {
 	fn area(&self) -> u32 {
 		self.width * self.height
 	}
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
