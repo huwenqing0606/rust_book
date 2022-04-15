c719c7a42da8ad02570051b984425bde751e64f4
huwenqing0606 <huwenqing.pku@gmail.com>
2022-04-15 10:08:11 -0500

    .

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,28 @@
+// enums example
+
+enum IpAddr {
+	V4(String),
+	V6(String),
+}
+
+
+impl IpAddr {
+	fn call(&self) -> &str {
+		match self {   
+			IpAddr::V4(string) => {println!("V4 = {}", string); "V4"}
+			IpAddr::V6(string) => {println!("V6 = {}", string); "V6"}
+		}
+	}
+}
+			
+			
 fn main() {
-    println!("Hello, world!");
+	let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+	
+	println!("home is {}", home.call());
+	println!("loopback is {}", loopback.call());
+
 }
