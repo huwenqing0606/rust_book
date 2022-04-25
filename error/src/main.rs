1f209712cc7801e93fcb565d45cafe4ff1e78bf3
huwenqing0606 <huwenqing.pku@gmail.com>
2022-04-25 13:34:22 -0500

    .

--- error/src/main.rs
+++ error/src/main.rs
@@ -1,11 +1,23 @@
 // example code for error handing
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io::{self, Read};
+use std::fs;
+
 
 fn main() {
     //use_panic();
 	//use_backtrace();
-	use_Result();
+	//use_Result_unwrap();
+	//use_Result_expect();
+	//read_username_from_file();
+	//read_username_from_file_short();
+	//read_username_from_file_short_short();
+	let result = last_char_of_first_line("");
+	match result {
+		Some(c) => println!("result is {}", c),
+		None => println!("not found!"),
+	};
 }
 
 
@@ -20,7 +32,7 @@ fn use_backtrace() {
 }
 
 
-fn use_Result() {
+fn use_Result_match() {
 	let f = File::open("hello.txt");
 	
 	match f 
@@ -35,4 +47,49 @@ fn use_Result() {
 				other_error => panic!("problem opening the file! {:?}", other_error)
 			}
 	};
+}
+
+
+fn use_Result_unwrap() {
+	let f = File::open("hello.txt").unwrap();
+}
+
+
+fn use_Result_expect() {
+	let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+
+fn read_username_from_file() -> Result<String, io::Error> {
+	let f = File::open("hello.txt");
+	
+	let mut f = match f {
+		Ok(file) => file,
+		Err(e) => return Err(e),
+	};
+	
+	let mut s = String::new();
+	
+	match f.read_to_string(&mut s) {
+		Ok(_) => Ok(s),
+		Err(e) => Err(e),
+	}
+}
+
+
+fn read_username_from_file_short() -> Result<String, io::Error> {
+	let mut s = String::new();
+	
+	File::open("hello.txt")?.read_to_string(&mut s)?;
+	Ok(s)
+}
+
+
+fn read_username_from_file_short_short() -> Result<String, io::Error> {
+	fs::read_to_string("hello.txt")
+}
+
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+	text.lines().next()?.chars().next()
 }
