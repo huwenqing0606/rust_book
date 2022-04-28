4224d43358e2995739bf76696f6816af88105748
huwenqing0606 <huwenqing.pku@gmail.com>
2022-04-28 14:27:50 -0500

    .

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -3,6 +3,8 @@
 fn main() {
     //largest_simple();
 	largest_duplicate();
+	use_struct();
+	use_trait();
 }
 
 
@@ -33,6 +35,10 @@ fn largest_duplicate() {
 	let result = largest(&number_list);
 	println!("largest number is {}", result);
 	
+	let char_list = vec!['a', 's', 'd', 'f'];
+	//let result = largest_generic(&char_list);
+	//println!("largest char is {}", result)
+	
 }
 
 
@@ -49,3 +55,112 @@ fn largest(list: &[i32]) -> i32 {
 	
 	largest
 }
+
+
+//fn largest_generic<T>(list: &[T]) -> T {
+//	
+//	let mut largest = list[0];
+//	
+//	for &item in list {
+//		if item > largest {
+//			largest = item;
+//		};
+//		println!("{}", item);
+//	}
+//	
+//	largest
+//}
+
+
+struct Point<T,U> {
+	x: T,
+	y: U,
+}
+
+impl<A, U> Point<A, U> {
+	fn x_T(&self) -> &A {
+		&self.x
+	}
+}
+
+impl Point<f32, f32> {
+	fn x_f32(&self) -> &f32 {
+		&self.x
+	}
+}
+
+impl<x1, y1> Point<x1, y1> {
+	fn mixup<x2, y2>(self, other: Point<x2, y2>) -> Point<x1, y2> {
+		Point{x: self.x, y: other.y}
+	}
+}
+
+fn use_struct() {
+	let both_integer = Point{x: 5, y: 10};
+	let both_float = Point{x: 1.0, y: 4.0};
+	let integer_and_float = Point{x: 5, y: 4.0};
+	println!("integer_and_float.x = {}", integer_and_float.x_T());
+	println!("both_float.x = {}", both_float.x_f32());
+	let mix = both_float.mixup(both_integer);
+	println!("mixup = {} {}", mix.x, mix.y);
+}
+
+
+pub struct NewsArticle {
+	pub headline: String,
+	pub location: String,
+	pub author: String,
+	pub content: String,
+}
+
+pub struct Tweet {
+	pub username: String,
+	pub content: String,
+	pub reply: bool,
+	pub retweet: bool,
+}
+
+pub trait Summary {
+	fn summarize(&self);
+}
+
+impl Summary for NewsArticle {
+	fn summarize(&self) {
+		println!("headline {} by {} ({})", self.headline, self.author, self.location);
+	}
+}
+
+impl Summary for Tweet {
+	fn summarize(&self) {
+		println!("tweet {}: {}", self.username, self.content);
+	}
+}
+
+fn use_trait() {
+	//use aggregator::{Summary, Tweet}; ??
+	let tweet = Tweet {
+		username: String::from("Dr_HuWenqing"),
+		content: String::from("Hello"),
+		reply: false,
+		retweet: false,
+	};
+	
+	tweet.summarize();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
