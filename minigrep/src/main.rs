6a92bbaefb201e37890432f433574f16a73a14f4
huwenqing0606 <huwenqing.pku@gmail.com>
2022-05-13 20:22:16 -0500

    .

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,23 +2,30 @@
 
 use std::env;
 use std::process;
+use std::io::prelude::*;
 
 use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+	let mut stderr = std::io::stderr();
 	
 	let config = Config::new(&args).unwrap_or_else(|error|
     {
-        println!("Problem parsing results: {}", error);
+        writeln!(
+			&mut stderr,
+			"Problem parsing arguments: {}",
+			error
+			).expect("Could not write to stderr");
         process::exit(1);
     });
-	
-	println!("Searching is for '{}'", config.query);
-	println!("In file '{}'", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        writeln!(
+			&mut stderr,
+			"Application error: {}", 
+			e
+			).expect("Could not write to stderr");
         process::exit(1);
     }
 }
