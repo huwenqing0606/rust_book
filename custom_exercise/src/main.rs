9186b42745a35ce56dea69a91e453b0921986cab
huwenqing0606 <huwenqing.pku@gmail.com>
2022-05-17 18:00:16 -0500

    .

---
+++ custom_exercise/src/main.rs
@@ -0,0 +1,36 @@
+use std::thread;
+use std::time::Duration;
+
+fn generate_workout(intensity: i32, random_number: i32)
+{
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!", 
+            expensive_closure(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_closure(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!")
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_closure(intensity)
+            )
+        }
+    }
+}
+
+
+fn main() {
+    generate_workout(100, 2);
+}
