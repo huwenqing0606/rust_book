acbf7899411b136b29794c95e501343fbc327ad6
huwenqing0606 <huwenqing.pku@gmail.com>
2022-05-22 09:45:39 -0500

    .

--- custom_exercise/src/main.rs
+++ custom_exercise/src/main.rs
@@ -1,22 +1,54 @@
 use std::thread;
 use std::time::Duration;
 
+
+struct Cacher<T>
+	where T: Fn(i32) -> i32 
+{
+		calculation: T,
+		value: Option<i32>,
+}
+	
+
+impl<T> Cacher<T> 
+	where T: Fn(i32) -> i32 
+{
+	fn new(calculation: T) -> Cacher<T> {
+		Cacher {
+			calculation,
+			value: None,
+		}
+	}
+	
+	fn value(&mut self, arg: i32) -> i32 {
+		match self.value {
+			Some(v) => v,
+			None => {
+				let v = (self.calculation)(arg);
+				self.value = Some(v);
+				v
+			},
+		}
+	}
+}
+
+
 fn generate_workout(intensity: i32, random_number: i32)
 {
-    let expensive_closure = |num: i32| -> i32 {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!", 
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -24,7 +56,7 @@ fn generate_workout(intensity: i32, random_number: i32)
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_result.value(intensity)
             )
         }
     }
@@ -33,4 +65,18 @@ fn generate_workout(intensity: i32, random_number: i32)
 
 fn main() {
     generate_workout(100, 2);
+}
+
+
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn call_with_different_values() {
+		use crate::Cacher;
+		let mut c = Cacher::new(|a| a);
+		let v1 = c.value(1);
+		let v2 = c.value(2);
+		assert_eq!(v2, 2);
+	}
 }
