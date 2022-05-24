9dfb07759c06c74f0bc5cc77f4810e5fa155f40f
huwenqing0606 <huwenqing.pku@gmail.com>
2022-05-24 09:36:53 -0500

    .

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -10,6 +10,20 @@ fn main() {
 }
 
 
+#[derive(PartialEq, Debug)]
+struct Shoe {
+	size: i32,
+	style: String,
+}
+
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: i32) -> Vec<Shoe> {
+	shoes.into_iter()
+		 .filter(|s| s.size == shoe_size)
+		 .collect()
+}
+
+
 #[test]
 fn iterator_demonstration() {
     let v1 = vec![1, 2, 3];
@@ -33,4 +47,23 @@ fn produce_other_iterator() {
     let v1: Vec<i32> = vec![1, 2, 3];
     let v2: Vec<_> = v1.iter().map(|x| x+1).collect();
     assert_eq!(v2, vec![2, 3, 4]);
+}
+
+#[test]
+fn filters_by_size() {
+	let shoes = vec![
+		Shoe {size: 10, style: String::from("sneaker")}, 
+		Shoe {size: 13, style: String::from("sandal")},
+		Shoe {size: 10, style: String::from("boot")},
+		];
+	
+	let in_my_size = shoes_in_my_size(shoes, 10);
+	
+	assert_eq!(
+		in_my_size,
+		vec![
+			Shoe {size: 10, style: String::from("sneaker")},
+			Shoe {size: 10, style: String::from("boot")},
+			]
+		);
 }
