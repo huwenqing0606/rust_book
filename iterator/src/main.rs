cc8f25a1f4be255ffa285f5d9fedb74425a1b183
huwenqing0606 <huwenqing.pku@gmail.com>
2022-05-26 12:13:31 -0500

    .

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -16,12 +16,38 @@ struct Shoe {
 	style: String,
 }
 
-
 fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: i32) -> Vec<Shoe> {
 	shoes.into_iter()
 		 .filter(|s| s.size == shoe_size)
 		 .collect()
 }
+
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter {count: 0}
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+
 
 
 #[test]
@@ -68,3 +94,15 @@ fn filters_by_size() {
 		);
 }
 
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+   
+}
