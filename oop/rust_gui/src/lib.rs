1246473e9e7627da2fbe093e485aa70d59f26a6d
huwenqing0606 <huwenqing.pku@gmail.com>
2022-06-09 18:00:10 -0500

    .

---
+++ oop/rust_gui/src/lib.rs
@@ -0,0 +1,29 @@
+// trait objects
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>, // use a trait object
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
--- oop/src/main.rs
+++ oop/src/main.rs
@@ -1,5 +1,21 @@
 extern crate averaged_collection;
+extern crate rust_gui;
 use averaged_collection::AveragedCollection;
+use rust_gui::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to draw a select box
+    }
+}
+
+use rust_gui::{Screen, Button};
 
 fn main() {
     println!("Object-Oriented Programming in Rust: Examples");
@@ -12,4 +28,25 @@ fn main() {
     println!("{:?}, average = {}", ac, ac.average());
     ac.remove();
     println!("{:?}, average = {}", ac, ac.average());
+
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
 }
