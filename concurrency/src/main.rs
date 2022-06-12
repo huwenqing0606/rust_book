806dd9bce6ab4b626602c7009e1d5df256990e5d
huwenqing0606 <huwenqing.pku@gmail.com>
2022-06-11 21:21:49 -0500

    .

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,3 +1,19 @@
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+
+    let ten_millis = time::Duration::from_millis(10);
+
+    let handle = thread::spawn(|| {
+        for i in 1..20 {
+            println!("hi number {} from the spawn thread", i);
+        }
+    });
+
+    for i in 1..10 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(ten_millis);
+    }
+
+    handle.join();
 }
